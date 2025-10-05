737f0a8916941cc6f9b0ec1ec493abd35e361bfb
4lvie <alvarojiba6106@gmail.com>
2025-10-05 20:33:33 +0200

    use homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Home from "./pages/Home";
-import Vault from "./pages/Vault";
-import ExploreVault from "./pages/ExploreVault";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
-import Layout from "./components/ui/Layout";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { TaskProvider } from "./context/TaskContext";
 
 const root = document.getElementById("root");
@@ -18,17 +8,7 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <TaskProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="vault" element={<Vault />} />
-          <Route path="explorevault" element={<ExploreVault />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <App/>
     </TaskProvider>
   </BrowserRouter>
 );
