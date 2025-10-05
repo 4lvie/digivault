1dd43140ebba401562ce61c71581af26b6db5d65
4lvie <alvarojiba6106@gmail.com>
2025-10-05 22:17:19 +0200

    delete missing vault

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Vault from "./pages/Vault";
 import ExploreVault from "./pages/ExploreVault";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -14,7 +13,6 @@ function App() {
     <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="vault" element={<Vault />} />
           <Route path="explorevault" element={<ExploreVault />} />
           <Route path="login" element={<Login />} />
           <Route path="profile" element={<Profile />} />
