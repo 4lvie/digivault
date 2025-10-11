c3beb83ea1e8d0716300d919bd3961ef38015cb2
4lvie <alvarojiba6106@gmail.com>
2025-10-11 16:37:22 +0200

    add authform in signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import Button from "../components/ui/Button";
 import Card from "../components/ui/Card";
 import LinkButton from "../components/ui/LinkButton";
 import HomeButton from "../components/ui/HomeButton";
+import AuthForm from "../components/forms/AuthForm";
 
 /**
  * Sign up page component for user registration
@@ -55,32 +56,11 @@ function SignUp() {
         <h2 className="text-3xl font-bold text-center text-green-700 mb-6">
           Create Account
         </h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <input
-            type="email"
-            placeholder="youremail@site.com"
-            className="input input-bordered input-info w-full"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="your-password"
-            className="input input-bordered input-info w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <Button variant="primary" type="submit" className="px-6 py-3 flex-1">
-            Sign Up
-          </Button>
-          {errorMsg && (
-            <div className="alert alert-error mt-2 py-2 px-4 text-sm">
-              {errorMsg}
-            </div>
-          )}
-        </form>
+        <AuthForm
+          onSubmit={handleSubmit}
+          buttonName={"Sign Up"}
+          errorMsg={errorMsg}
+        />
         <div className="mt-6 text-center text-gray-500 text-sm flex flex-col justify-center items-center">
           <p>Already have an account?</p>
           <LinkButton to="/login" className="link link-primary">
